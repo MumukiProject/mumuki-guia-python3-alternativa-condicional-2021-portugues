503d1f3b96407cad6b9abb18d98839b4e7d2c47b
rocio@mumuki.org <rocio@mumuki.org>
2022-08-24 16:03:23 +0000

    Mumuki Export on 2022-08-24 16:03:23 +0000

--- 00009_Os prêmios/test.py
+++ 00009_Os prêmios/test.py
@@ -1,12 +1,12 @@
 
   def test_medalla_segun_puesto_1(self):
-    self.assertEqual(medalha_segundo_colocacao(1), "oro")
+    self.assertEqual(medalha_segundo_colocacao(1), "ouro")
 
   def test_medalla_segun_puesto_2(self):
-    self.assertEqual(medalha_segundo_colocacao(2), "plata")
+    self.assertEqual(medalha_segundo_colocacao(2), "prata")
 
   def test_medalla_segun_puesto_3(self):
-    self.assertEqual(medalha_segundo_colocacao(3), "bronce")
+    self.assertEqual(medalha_segundo_colocacao(3), "bronze")
 
   def test_medalla_segun_puesto_4(self):
     self.assertEqual(medalha_segundo_colocacao(4), "nada")
