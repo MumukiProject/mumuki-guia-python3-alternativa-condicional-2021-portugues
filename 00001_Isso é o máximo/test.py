117a8274e54bb9f7f9bbebc19a2ed3e0ae0b882e
rocio@mumuki.org <rocio@mumuki.org>
2022-09-20 18:24:28 +0000

    Mumuki Export on 2022-09-20 18:24:28 +0000

--- 00001_Isso é o máximo/test.py
+++ 00001_Isso é o máximo/test.py
@@ -1,17 +1,17 @@
 
-  def test_maximo_4_10_e_10(self):
+  def test_máximo_4_10_é_10(self):
     self.assertEqual(maximo(4, 10), 10)
 
-  def test_maximo_3_10_e_10(self):
+  def test_máximo_3_10_é_10(self):
     self.assertEqual(maximo(3, 10), 10)
 
-  def test_maximo_2_10_e_10(self):
+  def test_máximo_2_10_é_10(self):
     self.assertEqual(maximo(2, 10), 10)
 
-  def test_maximo_20_10_e_20(self):
+  def test_máximo_20_10_é_20(self):
     self.assertEqual(maximo(20, 10), 20)
 
-  def test_maximo_20_15_e_20(self):
+  def test_máximo_20_15_é_20(self):
     self.assertEqual(maximo(20, 15), 20)
 
 
