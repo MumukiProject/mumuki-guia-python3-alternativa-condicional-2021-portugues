c6ecf9e303bdb5176a49c6c71acdd3dcd0da468b
rocio@mumuki.org <rocio@mumuki.org>
2022-09-20 17:17:59 +0000

    Mumuki Export on 2022-09-20 17:17:59 +0000

--- 00003_Cara ou coroa/test.py
+++ 00003_Cara ou coroa/test.py
@@ -1,14 +1,14 @@
   
-  def test_decision_con_moneda_cara_pizzas_empanadas_es_pizzas(self):
+  def test_decisão_com_moeda_cara_pizzas_empanadas_é_pizzas(self):
     self.assertEqual(decisao_com_moeda("cara", "pizzas", "empanadas"), "pizzas")
 
-  def test_decision_con_moneda_cara_asado_empanadas_es_asado(self):
+  def test_decisão_com_moeda_cara_churrasco_empanadas_é_churrasco(self):
     self.assertEqual(decisao_com_moeda("cara", "asado", "empanadas"), "asado")
 
-  def test_decision_con_moneda_ceca_pizzas_empanadas_es_empanadas(self):
+  def test_decisão_com_moeda_coroa_pizzas_empanadas_é_empanadas(self):
     self.assertEqual(decisao_com_moeda("ceca", "pizzas", "empanadas"), "empanadas")
 
-  def test_decision_con_moneda_ceca_pizzas_pastas_es_pastas(self):
+  def test_decisão_com_moeda_coroa_pizzas_massas_é_massas(self):
     self.assertEqual(decisao_com_moeda("ceca", "pizzas", "pastas"), "pastas")
 
 
