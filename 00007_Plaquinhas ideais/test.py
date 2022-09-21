8624b63e5fe0999fd68f08a6745bb9d1a04da115
rocio@mumuki.org <rocio@mumuki.org>
2022-09-21 15:40:31 +0000

    Mumuki Export on 2022-09-21 15:40:31 +0000

--- 00007_Plaquinhas ideais/test.py
+++ 00007_Plaquinhas ideais/test.py
@@ -1,12 +1,12 @@
   
-  def test_escribe_un_cartelito_largo_cuando_el_nombre_es_carla_toledo(self):
+  def test_escreva_uma_plaquinha_longa_quando_o_nome_é_carla_toledo(self):
     self.assertEqual(escrever_plaquinha_ideal("Ing.", "Carla", "Toledo"), "Ing. Carla Toledo")
   
-  def test_escribe_un_cartelito_largo_cuando_el_nombre_es_branco_luis(self):
+  def test_escreva_uma_plaquinha_longa_quando_o_nome_é_branco_luis(self):
     self.assertEqual(escrever_plaquinha_ideal("Ing.", "Branco", "Luis"), "Ing. Branco Luis")
   
-  def test_escribe_un_cartelito_corto_cuando_el_nombre_es_estanislao_schwarzschild(self):
+  def test_escreva_uma_plaquinha_curta_quando_o_nome_é_estanislao_schwarzschild(self):
     self.assertEqual(escrever_plaquinha_ideal("Dr.", "Estanislao", "Schwarzschild"), "Dr. Schwarzschild")
   
-  def test_escribe_un_cartelito_corto_cuando_el_nombre_es_katherine_boumann(self):
+  def test_escreva_uma_plaquinha_curta_quando_o_nome_é_katherine_boumann(self):
     self.assertEqual(escrever_plaquinha_ideal("Ing.", "Katherine", "Boumann"), "Ing. Boumann")
