99857dc61f7632fcc042141d5645503f1f4c4545
rocio@mumuki.org <rocio@mumuki.org>
2022-09-21 15:46:32 +0000

    Mumuki Export on 2022-09-21 15:46:32 +0000

--- 00007_Plaquinhas ideais/test.py
+++ 00007_Plaquinhas ideais/test.py
@@ -1,12 +1,12 @@
   
   def test_escreva_uma_plaquinha_longa_quando_o_nome_é_carla_toledo(self):
-    self.assertEqual(escrever_plaquinha_ideal("Ing.", "Carla", "Toledo"), "Enga. Carla Toledo")
+    self.assertEqual(escrever_plaquinha_ideal("Enga.", "Carla", "Toledo"), "Enga. Carla Toledo")
   
   def test_escreva_uma_plaquinha_longa_quando_o_nome_é_branco_luis(self):
-    self.assertEqual(escrever_plaquinha_ideal("Ing.", "Branco", "Luis"), "Eng. Branco Luis")
+    self.assertEqual(escrever_plaquinha_ideal("Eng.", "Branco", "Luis"), "Eng. Branco Luis")
   
   def test_escreva_uma_plaquinha_curta_quando_o_nome_é_estanislao_schwarzschild(self):
     self.assertEqual(escrever_plaquinha_ideal("Dr.", "Estanislao", "Schwarzschild"), "Dr. Schwarzschild")
   
   def test_escreva_uma_plaquinha_curta_quando_o_nome_é_katherine_boumann(self):
-    self.assertEqual(escrever_plaquinha_ideal("Ing.", "Katherine", "Boumann"), "Enga. Boumann")
+    self.assertEqual(escrever_plaquinha_ideal("Enga.", "Katherine", "Boumann"), "Enga. Boumann")
