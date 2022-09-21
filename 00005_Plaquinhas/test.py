c508da838fdb1a0021408e63c0fa5230f00a4983
rocio@mumuki.org <rocio@mumuki.org>
2022-09-21 15:37:15 +0000

    Mumuki Export on 2022-09-21 15:37:15 +0000

--- 00005_Plaquinhas/test.py
+++ 00005_Plaquinhas/test.py
@@ -1,7 +1,7 @@
   
-  def test_escribir_cartelito_Dra_Ana_Perez_es_Dra_Ana_Perez(self):
+  def test_escrever_plaquinha_Dra_Ana_Perez_é_Dra_Ana_Perez(self):
     self.assertEqual(escrever_plaquinha("Dra.", "Ana", "Perez"), "Dra. Ana Perez")
   
-  def test_escribir_cartelito_Dr_Julio_Gelman_es_Dr_Julio_Gelman(self):
+  def test_escrever_plaquinha_Dr_Julio_Gelman_é_Dr_Julio_Gelman(self):
     self.assertEqual(escrever_plaquinha("Dr.", "Julio", "Gelman"), "Dr. Julio Gelman")
 
