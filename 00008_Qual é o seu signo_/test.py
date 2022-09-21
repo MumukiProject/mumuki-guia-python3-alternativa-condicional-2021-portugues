d6d0a2a7a7ccd9c8cd44e0f537d3f16128fa6076
rocio@mumuki.org <rocio@mumuki.org>
2022-09-21 15:48:35 +0000

    Mumuki Export on 2022-09-21 15:48:35 +0000

--- 00008_Qual é o seu signo_/test.py
+++ 00008_Qual é o seu signo_/test.py
@@ -1,17 +1,17 @@
 
-  def test_signo_10_es_1(self):
+  def test_signo_10_é_1(self):
     self.assertEqual(signo(10), 1)
 
-  def test_signo_1_es_1(self):
+  def test_signo_1_é_1(self):
     self.assertEqual(signo(1), 1)
 
-  def test_signo_0_es_0(self):
+  def test_signo_0_é_0(self):
     self.assertEqual(signo(0), 0)
 
-  def test_signo_menos_65_es_menos_1(self):
+  def test_signo_menos_65_é_menos_1(self):
     self.assertEqual(signo(-65), -1)
 
-  def test_signo_menos_87_es_menos_1(self):
+  def test_signo_menos_87_é_menos_1(self):
     self.assertEqual(signo(-87), -1)
 
 
