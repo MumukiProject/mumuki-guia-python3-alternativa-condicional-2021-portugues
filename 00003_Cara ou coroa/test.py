5536bf8c274a44c7a0afddccc23514e180931d16
nadia@mumuki.org <nadia@mumuki.org>
2023-01-06 12:55:25 +0000

    Mumuki Export on 2023-01-06 12:55:25 +0000

--- 00003_Cara ou coroa/test.py
+++ 00003_Cara ou coroa/test.py
@@ -1,14 +1,14 @@
   
-  def test_decisão_com_moeda_cara_pizzas_empanadas_é_pizzas(self):
-    self.assertEqual(decisao_com_moeda("cara", "pizzas", "empanadas"), "pizzas")
+  def test_decisão_com_moeda_cara_pizza_empanado_é_pizza(self):
+    self.assertEqual(decisao_com_moeda("cara", "pizza", "empanado"), "pizza")
 
-  def test_decisão_com_moeda_cara_churrasco_empanadas_é_churrasco(self):
-    self.assertEqual(decisao_com_moeda("cara", "asado", "empanadas"), "asado")
+  def test_decisão_com_moeda_cara_churrasco_empanado_é_churrasco(self):
+    self.assertEqual(decisao_com_moeda("cara", "churrasco", "empanado"), "churrasco")
 
-  def test_decisão_com_moeda_coroa_pizzas_empanadas_é_empanadas(self):
-    self.assertEqual(decisao_com_moeda("ceca", "pizzas", "empanadas"), "empanadas")
+  def test_decisão_com_moeda_coroa_pizza_empanado_é_empanado(self):
+    self.assertEqual(decisao_com_moeda("ceca", "pizza", "empanado"), "empanado")
 
-  def test_decisão_com_moeda_coroa_pizzas_massas_é_massas(self):
-    self.assertEqual(decisao_com_moeda("ceca", "pizzas", "pastas"), "pastas")
+  def test_decisão_com_moeda_coroa_pizza_massas_é_massas(self):
+    self.assertEqual(decisao_com_moeda("ceca", "pizza", "massas"), "massas")
 
 
